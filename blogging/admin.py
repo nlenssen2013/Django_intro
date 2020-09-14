ebf9d17abefb5570cdfd696c372c0ce658cb40f5
Nick Lenssen <nicklenssen4@gmail.com>
2020-09-13 23:13:58 -0400

    Blogging Admin now has Posts may have categores

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 
 from blogging.models import Post, Category
 
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'author',
+                    'created_date', 'published_date')
+    list_filter = ('created_date',)
+    date_hierarchy = 'created_date'
+    ordering = ('-created_date',)
+    fields = ('title', 'text', 'author', )
+    inlines = [CategoryInline, ]
+    exclude = ('posts',)
+
 admin.site.register(Post)
 admin.site.register(Category)
